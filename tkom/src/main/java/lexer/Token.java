18f9812f53a4a4e7ca4046915a61025aa8f5a3a2
tedtheripper <lemon.juice0731@gmail.com>
2022-05-03 16:47:39 +0200

    added "is" method

--- tkom/src/main/java/lexer/Token.java
+++ tkom/src/main/java/lexer/Token.java
@@ -1,4 +1,8 @@
 package lexer;
 
 public record Token(TokenType type, Position position, Object value) {
+
+    public boolean is(TokenType otherType) {
+        return this.type == otherType;
+    }
 }
